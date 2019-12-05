83ea19072bb946053a3c2158010f8393b58459f5
test1 <test1@SFCLASS14.training.local>
2019-12-05 19:22:48 +0200

    [commit messege]

--- src/main/GetResponse.java
+++ src/main/GetResponse.java
@@ -83,35 +83,35 @@ import json.*;
 
 //CASE 3 , HTTPS CONNECTION RESULTS
 
-//public class GetResponse
-//{
-//	public static void main(String[] args) throws IOException {
-//		GetResponse.getResponse();
-//	}
-//
-//	private static String getResponse() 
-//	{	
-//		try {
-//				String myUrl = "https://google.com";
-//				String url = (myUrl);
-//				URL obj = new URL(url);
-//				HttpsURLConnection conn = (HttpsURLConnection) obj.openConnection();
-//				conn.setRequestMethod("GET");
-//				BufferedReader in = new BufferedReader( new InputStreamReader( conn.getInputStream() ));
-//				String InputLine;
-//				StringBuffer response = new StringBuffer();
-//				while ((InputLine = in.readLine()) != null) {
-//				response.append(InputLine);
-//				}    
-//				System.out.println(response.toString());
-//				return response.toString();
-//  }
-//		catch (IOException ex) 
-//		{
-//			ex.printStackTrace();
-//		}
-//		return null;
-//  }
-//}
+public class GetResponse
+{
+	public static void main(String[] args) throws IOException {
+		GetResponse.getResponse();
+	}
+
+	private static String getResponse() 
+	{	
+		try {
+				String myUrl = "https://google.com";
+				String url = (myUrl);
+				URL obj = new URL(url);
+				HttpsURLConnection conn = (HttpsURLConnection) obj.openConnection();
+				conn.setRequestMethod("GET");
+				BufferedReader in = new BufferedReader( new InputStreamReader( conn.getInputStream() ));
+				String InputLine;
+				StringBuffer response = new StringBuffer();
+				while ((InputLine = in.readLine()) != null) {
+				response.append(InputLine);
+				}    
+				System.out.println(response.toString());
+				return response.toString();
+  }
+		catch (IOException ex) 
+		{
+			ex.printStackTrace();
+		}
+		return null;
+  }
+}
 
 
