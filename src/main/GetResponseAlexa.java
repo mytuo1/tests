145c65fe77ffc03f108295dacb40bfa9b55f7b0c
noizzex <noizzex@gmail.com>
2019-12-07 21:35:02 +0200

    new commit

--- src/main/GetResponseAlexa.java
+++ src/main/GetResponseAlexa.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
 
 public class GetResponseAlexa
 {
 	public static void main(String[] args) throws IOException {
 			GetResponseAlexa.getResponse();
+			GetResponseAlexa.getCurrentTime();
+			GetResponseAlexa.getNextSunday();
 	}
 
 	private static String getResponse() 
@@ -34,4 +42,20 @@ public class GetResponseAlexa
 			}
 	return null;
     }
+	
+	private static int getCurrentTime() {		
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println("The current date and time is: " + sdf.format(calendar.getTime()) );
+		return 0;
+	}
+	
+	public static int getNextSunday() {
+	    LocalDate dateTime = LocalDate.now();
+	    LocalDate getNextSunday = dateTime.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+	    LocalDate finalDate = getNextSunday.plusWeeks(1);
+	    String formattedDate = finalDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+	    System.out.println("The next Sunday is: " + formattedDate);
+	    return 1;
+	}
 }
